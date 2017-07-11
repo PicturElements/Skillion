0835f26483a1537c05b5b90b6a5f967a6bbe2d40
William Hicks <will.hicks917@gmail.com>
2017-07-11 15:44:00 -0500

    Update app.py

--- app.py
+++ app.py
@@ -241,6 +241,7 @@ def editProfile():
             getUrl = baseUrl + str(user['token'])
             request = requests.post(getUrl)
             response = json.loads(request.content)
+	    return "{}".format(response)
             if response['addresses'][0]['address_type'].lower() == "actual":
                 actual_address = response['addresses'][0]
                 postal_address = response['addresses'][1]
