9dd89cd3bfdd5232bdf325ec50e0840ab827cdc8
William Hicks <will.hicks917@gmail.com>
2017-07-15 22:45:43 -0500

    Update app.py

--- app.py
+++ app.py
@@ -760,6 +760,7 @@ def guestMode(a_token, nextmode):
 	GET_URL = GLOBAL_BASE_URL + '/REST-Customer.awp?Procedure=Product_Details&Token=' + token
 	data = requests.post(GET_URL)
 	out_of_stock = json.loads(data.content)['product'][0]['out_of_stock']
+	return "{}".format(data.content.decode('utf-8'))
 	if out_of_stock in [True, 'true']:
 		return render_template('errors/outofstock.html')
         if nextmode == "outright":
